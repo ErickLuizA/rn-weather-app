54a4fbc1f525267727c13a36fb02b2ad66f2e588
erickLuizA <erickluiz69@gmail.com>
2021-08-03 23:04:44 -0300

    feat: add getCUrrentWeather to api methods

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,40 @@
 import axios from 'axios'
+import { WeatherData } from '../models/Weather'
 
-const api = axios.create({
-  baseURL: 'http://api.openweathermap.org/data/2.5/',
-  params: {
-    appid: process.env.API_KEY,
-  },
-})
+const BASE_URL = 'http://api.openweathermap.org/data/2.5'
 
-export default api
+type ByCity = {
+  type: 'city'
+  cityName: string
+}
+
+type ByGeo = {
+  type: 'geo'
+  latitude: number
+  longitude: number
+}
+
+type IGetCurrentWeatherParams = {
+  params: ByCity | ByGeo
+}
+
+export async function getCurrentWeather({ params }: IGetCurrentWeatherParams) {
+  const locationParams =
+    params.type === 'city'
+      ? {
+          q: params.cityName,
+        }
+      : {
+          lat: params.latitude,
+          lon: params.longitude,
+        }
+
+  const response = await axios.get<WeatherData>(`${BASE_URL}/weather`, {
+    params: {
+      ...locationParams,
+      appid: process.env.API_KEY,
+    },
+  })
+
+  return response.data
+}
